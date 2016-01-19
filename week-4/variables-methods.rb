1dd460c3f2278a84d768f43422bae69274e431a3
konichiwa1200 <konichiwa1200@gmail.com>
2016-01-19 02:31:36 -0800

    challenge 4.3 completed

--- week-4/variables-methods.rb
+++ week-4/variables-methods.rb
@@ -25,3 +25,19 @@ bigger
 
 # Challenge 4.3.1 link https://github.com/konichiwa1200/phase-0/blob/master/week-4/address/my_solution.rb
 # Challenge 4.3.1 link https://github.com/konichiwa1200/phase-0/blob/master/week-4/math/my_solution.rb
+
+# How do you define a local variable?
+	# Simply naming the variable and initializing it to the value of choice
+# How do you define a method?
+	# Using the keyword "def" followed by the name of the method and paramaters that it will take, if any, and finishing it with an "end" keyword
+# What is the difference between a local variable and a method?
+	# A variable only stores data, while a method is a sequence of code that will return an evaluated output
+# How do you run a ruby program from the command line?
+	# Type: ruby <file_name>
+	# Must be *.rb file extension
+# How do you run an RSpec file from the command line?
+	# Type: rspec <file_name>
+	# Must be *.rb file extension
+	# rspec gem must be installed in ruby environment
+# What was confusing about this material? What made sense?
+	# Nothing was confusing thus far. Everything was a great refresher course.
