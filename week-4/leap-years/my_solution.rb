4fb0f3c233af330a2408aadd70d8f1b8cc9ee246
konichiwa1200 <konichiwa1200@gmail.com>
2016-01-19 17:09:24 -0800

    challenge 4.5.4 complete

--- week-4/leap-years/my_solution.rb
+++ week-4/leap-years/my_solution.rb
@@ -6,14 +6,8 @@
 # Your Solution Below
 def leap_year?(year)
 	leap = false
-	if year % 4 == 0 
-		leap = true
-		if year % 100 == 0
-			leap = false
-			if year % 400 == 0
-				leap = true
-			end
-		end
-	end
+	leap = true if year % 4 == 0 
+	leap = false if year % 100 == 0
+	leap = true if year % 400 == 0
 	leap
 end
