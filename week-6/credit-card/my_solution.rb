728d83e3eeab70d1dd3d37397ecf9589911d2ec5
konichiwa1200 <konichiwa1200@gmail.com>
2016-02-05 01:46:23 -0800

    challenge 6.7 complete

--- week-6/credit-card/my_solution.rb
+++ week-6/credit-card/my_solution.rb
@@ -1,14 +1,20 @@
 # Class Warfare, Validate a Credit Card Number
 
 
-# I worked on this challenge [by myself, with: ].
-# I spent [#] hours on this challenge.
+# I worked on this challenge [by myself, with: xin john zhang].
+# I spent [1] hours on this challenge.
 
 # Pseudocode
 
-# Input:
-# Output:
+# Input: integers
+# Output: boolean
 # Steps:
+	# test length of Input
+	# split input and place in array
+	# convert array elements to integer
+	# create second array containing double element input
+	# split second array into elements
+	# sum and check modulos and return boolean
 
 
 # Initial Solution
@@ -17,10 +23,32 @@
 # of exactly 16 digits
 
 class CreditCard
+	
+	# initializates the necessary variables needed and checks the length of the input
+	def initialize(number)
+		@original = number.to_s.split("")
+		@single = []
+		@double = []
 
+		unless @original.length  == 16
+			raise ArgumentError.new("Your input is too long or too short, please enter a correct input")
+		end
+	end
+
+	def check_card
+		# splits input into two arrays based on digit indexing
+		(0...16).each {|i| i % 2 == 0 ? @double << @original[i] : @single << @original[i]}
+
+		# manipulates digits for that need to be doubled and split any two digit outcomes
+		@double = @double.map{|element| element.to_i * 2}.join("").split("")
+
+		# checks the modulos of 10 of the final sum
+		(@double.map{|element| element.to_i}.reduce(:+) + @single.map{|element| element.to_i}.reduce(:+)) % 10 == 0
+	end
 end
 
-
+# check = CreditCard.new(4408041234567906)
+# p check.check_card
 
 # Refactored Solution
 
@@ -32,3 +60,9 @@ end
 
 
 # Reflection
+# What was the most difficult part of this challenge for you and your pair?
+# 	Originally we felt were getting the challenge of just spliting it, but forgot to divide the digits into to arrays. Once the mistake was resolved, the rest of the challenge was a breeze.
+# What new methods did you find to help you when you refactored?
+# 	The .reduce method was very helpful. It creates a clean and readable code.
+# What concepts or learnings were you able to solidify in this challenge?
+# 	The division of the modulos and the Argument Error testing.
