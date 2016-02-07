6451a8c1a58337a6dd8ee2bc7a20b80c7f266106
konichiwa1200 <konichiwa1200@gmail.com>
2016-02-07 02:05:37 -0800

    challenge 6.5 complete

--- week-6/nested_data_solution.rb
+++ week-6/nested_data_solution.rb
@@ -88,3 +88,11 @@ end
   
 
 p generator_three(startup_names)
+
+# # Reflect
+# What are some general rules you can apply to nested arrays?
+#   Test to see if they are arrays within arrays. If so, keep nesting your tests until you get to the most inner nested array. The number of test to perform equals the number of nested arrays. Keeping track of what each loop is being passed will have a great impact on your productivity.
+# What are some ways you can iterate over nested arrays?
+#   Using the each method, or the map method. We can also use the range method to iterate through the indexs and passing an integer to the variables to access each of the elemnts in the array. That method will alter the way that it would be accessed. Example is array[j][k][i], but this method, especially if copied to another array, will produce a flat array. Which is the last way that I would access it. Again, if I didnt care about the structure of the data, then flatting the array would be the fastest and easiest way. 
+# Did you find any good new methods to implement or did you re-use one you were already familiar with? What was it and why did you decide that was a good option?
+#   I stuck to the mighty .each method because it leaves a clean and clear outline of what I am doing. And when I was creating the variables for each loop, I gave them descriptive names to keep it clean and readable. The other methods that I looked at did not give me the control that I wanted. I did want to use the .flatten method, but I think the challenge wanted to keep the structure of the data in tacked. Finally, the use of recursion resulted in the most elegant and effective way to solve the bonus challenge.
