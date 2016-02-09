ca73799ba2661c0ff888a61aaaa44ab1a25eec94
konichiwa1200 <konichiwa1200@gmail.com>
2016-02-08 17:49:09 -0800

    challenge 7.2 init

---
+++ week-7/eloquent.js
@@ -0,0 +1,42 @@
+// Eloquent JavaScript
+
+// Run this file in your terminal using `node my_solution.js`. Make sure it works before moving on!
+// Run this file in your terminal using `node eloquent.js`. Make sure it works before moving on!
+
+// Program Structure
+// Write your own variable and do something to it.
+
+// var city = "San Diego";
+// console.log("America's Finest City: " + city);
+
+// // Program about food
+// function ask()
+// {
+//     prompt("What's your favorite food?");
+//     alert("Hey! That's my favorite too!");
+// }
+
+// ask();
+// Complete one of the exercises: Looping a Triangle, FizzBuzz, or Chess Board
+
+// Triangle
+// function triangle(size)
+// {
+//     var str = "#";
+    
+//     for (var i = 0; i < size; i++)
+//     {
+//         console.log(str);
+//         str += "#"
+//     }
+// }
+
+// triangle(5);
+// Functions
+
+// Complete the `minimum` exercise.
+
+
+
+// Data Structures: Objects and Arrays
+// Create an object called "me" that stores your name, age, 3 favorite foods, and a quirk below.
