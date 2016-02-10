2d13326d901a1876a99ba61af93d5615eaffc77d
konichiwa1200 <konichiwa1200@gmail.com>
2016-02-09 19:29:46 -0800

    Challenge 7.2 complete

--- week-7/eloquent.js
+++ week-7/eloquent.js
@@ -31,23 +31,50 @@
 //     }
 // }
  
-triangle(5);
+// triangle(5);
 
 // FizzBuzz
-function fizzbuzz()
-{
-    var array = new Array(100);
-    for (var i in array) 
-    {
-        console.log(i);
-    }
-} 
+// function fizzbuzz()
+// {
+//     var array = new Array(100);
+//     for (var i = 1; i < 101; i++) 
+//     {
+//         if (i % 3 == 0 && i % 5 == 0)
+//             {console.log("FizzBuzz");}
+//         else if (i % 3 == 0)
+//             {console.log("Fizz");}
+//         else if (i % 5 == 0)
+//             {console.log("Buzz");}
+//         else
+//             {console.log(i)}
+//     }
+// } 
 
-fizzbuzz();
+// fizzbuzz();
 
 // Complete the `minimum` exercise.
+// Mininum
+// function mininum(num1, num2)
+// {
+//     if (num1 < num2)
+//     {
+//         console.log(num1);
+//     }
+//     else 
+//     {
+//         console.log(num2);
+//     }
+// };
 
-
+// mininum(-120, -120);
 
 // Data Structures: Objects and Arrays
 // Create an object called "me" that stores your name, age, 3 favorite foods, and a quirk below.
+
+// Me
+var me = {
+    name: "Rene",
+    age: "Big", 
+    foods: ["Tacos", "Burritos", "Nachos"],
+    quirk: "Rules are more like guidelines philosophy"
+}
