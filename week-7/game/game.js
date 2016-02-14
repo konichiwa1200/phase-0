9d2d9a5347b2018ca4839feed0895dd112d749f5
konichiwa1200 <konichiwa1200@gmail.com>
2016-02-13 20:05:34 -0800

    layout is almost complete

--- week-7/game/game.js
+++ week-7/game/game.js
@@ -3,22 +3,32 @@
 // This is a solo challenge
 
 // Your mission description:
-// Overall mission:
-// Goals:
-// Characters:
-// Objects:
+// Overall mission: Select the corrent name of the picture
+// Goals: 
+// 1. Click tiles and make them disappear
+// 2. Keep count of clicks
+// 3. Return true of false if guess is correct
+//     3.1 show credits at the end
+
 // Functions:
+// Counter tracker
+// Return credits
+// Set links and image
 
 // Pseudocode
-//
-//
-//
-//
-//
+// Counter tracker
+// Return credits
+// Set links and image
 
 // Initial Code
-
-
+// function click_counter() {
+//     var id = document.addEventListener('click', function(x){
+        
+//     })
+// }
+function call_id() {
+    alert("This is the id" + this.id)
+}
 
 
 
