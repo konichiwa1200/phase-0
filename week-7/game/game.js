7207617a887f8d10b6231d1e00f6be247997999f
konichiwa1200 <konichiwa1200@gmail.com>
2016-02-14 22:49:34 -0800

    add coments to js code

--- week-7/game/game.js
+++ week-7/game/game.js
@@ -12,6 +12,7 @@
 
 // Functions:
 window.onload = function() {
+    // initialized the variables that will be used downstream
     var counter = 1
     var credits = ""
     var pictures = {
@@ -43,80 +44,113 @@ window.onload = function() {
             ],
     }
     
+    // setup the picture and website
     setup()
     
+    // register the click event, and pass info downstream
     document.addEventListener("click", function(event) {
         evaluate_guess(event.target.id)
-        update_counter(event.target.id)
     })
     
+    // update counter
     function update_counter(element_passed) {
         var element = document.getElementById("counter")
         var check_class = document.getElementById(element_passed)
+        // checks to see if we clicked a tile
         if (check_class.className == "tiles") {
             element.innerHTML = counter++
             update_tile(element_passed)
         }
     }
     
+    // update tile classes
     function update_tile(element_passed) {
         var update_element = document.getElementById(element_passed)
+        // make tile disappear
         if(update_element.className == "tiles") {
             update_element.className = "tiles hidden"
         }
     }
     
+    // function that sets up the webpage
     function setup() {
+        // randomize the possible links, there are 4 possibilities, thus value of 5
         for (var i = 1; i < 5; i++) {
             var random_int = getRandomIntInclusive(0,4)
             var picture_label = document.getElementById("link" + i)
             picture_label.innerHTML = pictures.short_description[random_int]
             picture_label.setAttribute("alt", pictures.short_description[random_int]) 
         }
+        // calls function that will set up the background picture
         img_setup()
     }
     
+    // function used for random integer selection
     function getRandomIntInclusive(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
     
+    // sets up background image
     function img_setup() {
+        // variable used for cleaner downstream code
         var random_picture_index = getRandomIntInclusive(1,4)
         var random_picture_src = document.getElementById("link" + random_picture_index).getAttribute("alt")
         var background_picture = document.getElementById("background_picture")
         var random_alt_index = pictures.short_description.indexOf(random_picture_src)
+        
+        // set the background picture to one of the possible answers on the right
         background_picture.setAttribute("alt", random_picture_src)
         background_picture.setAttribute("src", "img/" + pictures.address[random_alt_index])
+        
+        // sets the value for picture credits
         credits = pictures.credits[random_alt_index]
     }
     
+    // evaluates each click to determine if tile or guess was registered
     function evaluate_guess(element_passed) {
-        var img = document.getElementById(element_passed).getAttribute("alt")
-        var guess = document.getElementById("background_picture").getAttribute("alt")
-        if (img == guess) {
-            remove_tiles(element_passed, true)
+        // determines if a possible answer was clicked
+        if (element_passed == "link1" || element_passed == "link2" || element_passed == "link3" || element_passed == "link4") {
+            // variables for cleaner code
+            var img = document.getElementById(element_passed).getAttribute("alt")
+            var guess = document.getElementById("background_picture").getAttribute("alt")
+            // user anwered correctly
+            if (img == guess) {
+                remove_tiles(element_passed, true)
+            }
+            // user answered incorrectly
+            else {
+                remove_tiles(element_passed, false)
+            }
         }
+        // tile covering image was clicked
         else {
-            remove_tiles(element_passed, false)
+            update_counter(element_passed)
         }
     }
     
+    // function that removes visable tiles over image to reveal image
     function remove_tiles(element_passed, value) {
+        // loop through tiles and add the hidden class to tiles on top of image
         for (var i = 1; i < 26; i++) {
             var check_tiles = document.getElementById("tile-" + i) 
             if (check_tiles.className == "tiles") {
                 check_tiles.className = "tiles hidden"
             }
         }
+        // calls function to give credits for the image revealed
         give_credits(element_passed, value)
     }
     
+    // adds image credits to the webpage upon image reveal
     function give_credits(element_passed, value) {
+        // adds credit info to DOM
         document.getElementById("credit_picture").innerHTML = credits
+        // user answered correctly
         if (value == true) {
             document.getElementById("status").innerHTML = "Correct!"
             document.getElementById("credit_div").className = "correct_guess"
         }
+        // user answered incorrectly
         else {
             document.getElementById("status").innerHTML = "Incorrect!"
             document.getElementById("credit_div").className = "incorrect_guess"
