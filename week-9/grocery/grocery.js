3295cfc362bdfe9438ddb03cbe8ea12b6f4af288
konichiwa1200 <konichiwa1200@gmail.com>
2016-02-23 12:18:53 -0800

    HTML form created

---
+++ week-9/grocery/grocery.js
@@ -0,0 +1,13 @@
+// Pseudocode
+// place field with placeholder "item"
+// place field with placeholder "quantity"
+// place submit button
+
+// add items object called item
+//     property name will be item
+//     property value will be quantity
+
+// display list
+//     place remove button next to item displayed
+
+// initial
