b53be2c301888bf2811665c6130df7d94265f555
konichiwa1200 <konichiwa1200@gmail.com>
2016-02-24 18:17:32 -0800

    Challenge 9.3.1 complete

--- week-9/jquery/jquery_example.js
+++ week-9/jquery/jquery_example.js
@@ -17,18 +17,29 @@ $('body').css({'background-color': 'pink'})
 
 //RELEASE 2:
   //Add code here to select elements of the DOM
-
-
+$('img').click(function(){
+    $('img').fadeOut(3000)
+})
 //RELEASE 3:
   // Add code here to modify the css and html of DOM elements
-
+$('body h1:first').css({'color': 'white', 'border': '1px solid black', 'visibility': 'true'})
+$('h1:first-child').html('Sea Lions 2016')
 
 //RELEASE 4: Event Listener
   // Add the code for the event listener here
-
+$('img').on('mouseover', function(e){
+    e.preventDefault()
+    $(this).attr('src', 'https://i.ytimg.com/vi/aIbeA4HsTZ4/hqdefault.jpg')
+  })
+$('img').on('onmouseleave', function(){
+    return true
+})
 
 //RELEASE 5: Experiment on your own
-
+    
+    $('img').animate({
+        borderWidth: '1px'
+    })
 
 
 
